f2aed018fcacb260a2119b2efc287048e76d622d
Lance Bailey <lancebailey222@gmail.com>
2021-07-13 11:06:08 -0400

    initial setup

---
+++ public/server.js
@@ -0,0 +1,17 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
